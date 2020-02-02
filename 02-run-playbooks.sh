85bdde5563b99e2ed1a6a789e4af262770ece539
Sebastian Senf <contact@mustardamus.com>
2020-02-02 18:51:00 +0100

    desktop launchers

--- 02-run-playbooks.sh
+++ 02-run-playbooks.sh
@@ -1,5 +1,6 @@
 sudo ansible-playbook playbooks/sudo-passwordless.yml
 sudo ansible-playbook playbooks/install-packages.yml
+sudo ansible-playbook playbooks/remove-packages.yml
 ansible-playbook playbooks/git.yml
 sudo ansible-playbook playbooks/i3.yml
 ansible-playbook playbooks/theme.yml
@@ -11,5 +12,7 @@ sudo ansible-playbook playbooks/mongodb.yml
 ansible-playbook playbooks/robo3t.yml
 ansible-playbook playbooks/visual-studio-code.yml
 ansible-playbook playbooks/joplin.yml
+ansible-playbook playbooks/tor-browser.yml
 sudo ansible-playbook playbooks/auto-login.yml
 sudo ansible-playbook playbooks/lollypop.yml
+ansible-playbook playbooks/desktop-launchers.yml
